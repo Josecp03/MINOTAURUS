1c7da120cf9e70af91b365f89521160324f6a863
alfonsaco <alfonso.rincondev@gmail.com>
2024-09-02 17:49:47 +0200

    Se añade la canción del Inicio + Transición de audio al pulsar en Play

--- Script/menu.js
+++ Script/menu.js
@@ -1,12 +1,34 @@
-const playGame = document.querySelector(".play-game");
-let sonidoPlay = new Audio("../Audio/lego-breaking.mp3"); 
+window.addEventListener("DOMContentLoaded", function() {
+    // Botones
+    const playGame = document.querySelector(".play-game");
+    let sonidoPlay = new Audio("../Audio/lego-breaking.mp3"); 
+    
+    // Sonido de los botones
+    function reproducirSonido(sonido) {
+        sonido.play();
+    }
 
-// Función de los botones
-function reproducirSonido(sonido) {
-    sonido.play();
-}
+    // Música del menú
+    const menuTheme=new Audio("../Audio/Menu-Theme.mp3");
+    menuTheme.loop=true;
+    menuTheme.play();
+    let volumen=0.8;
 
-playGame.addEventListener("click", function () {
-    reproducirSonido(sonidoPlay);
-    playGame.classList.add("desaparecer-boton");
-}) 
+
+    // ANIMACIÓN COMENZAR JUEGO
+    playGame.addEventListener("click", function () {
+        reproducirSonido(sonidoPlay);
+        playGame.classList.add("desaparecer-boton");
+
+        // Reducir el audio lentamente, para hacerlo más fluido
+        let reducirAudio=setInterval(() => {
+            if(volumen > 0.05) {
+                volumen-=0.05;
+                menuTheme.volume=volumen;
+            } else {
+                menuTheme.pause();
+                clearInterval(reducirAudio);
+            }
+        }, 100);
+    }); 
+});
