3fa3535e0f46d0a39390ec8166c6e7b151b34979
alfonsaco <alfonso.rincondev@gmail.com>
2024-09-02 13:02:29 +0200

    Animación del botón Play

--- Script/menu.js
+++ Script/menu.js
@@ -1,11 +1,12 @@
 const playGame = document.querySelector(".play-game");
-
 let sonidoPlay = new Audio("../Audio/lego-breaking.mp3"); 
 
+// Función de los botones
 function reproducirSonido(sonido) {
     sonido.play();
 }
 
 playGame.addEventListener("click", function () {
     reproducirSonido(sonidoPlay);
+    playGame.classList.add("desaparecer-boton");
 }) 
